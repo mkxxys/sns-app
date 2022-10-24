acf725dc40bbbc9f9de6675fa32704e39253b74d
田中美心人 <jda5xx@gmail.com>
2022-10-24 18:08:36 +0900

    トップ画面の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def index
+      @users = User.all
+    end
+
     protected
   
     def configure_permitted_parameters
