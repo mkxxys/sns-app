8b0e3b82ea402687e06525cecc6c448152747486
田中美心人 <jda5xx@gmail.com>
2022-10-24 18:15:09 +0900

    トップ画面の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def index
-      @users = User.all
-    end
-
     protected
   
     def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#home'
+  # root "articles#index"
 end
